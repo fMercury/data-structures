b0a298c88c2ec123cdcdd2b23b87f620dab99efd
Franco ★ <fberdun@gmail.com>
2019-10-25 10:47:30 +0200

    Create TreeNode.java

---
+++ TreeNode.java
@@ -0,0 +1,36 @@
+public class TreeNode<E extends Comparable<E>> {
+
+  TreeNode<E> left, right;
+  E value;
+
+  public TreeNode(E value) {
+    left = right = null;
+    this.value = value;
+  }
+
+  public E getValue() {
+    return value;
+  }
+
+  public TreeNode<E> getLeft() {
+    return left;
+  }
+
+  public void setLeft(TreeNode<E> node) {
+    left = node;
+  }
+
+  public TreeNode<E> getRight() {
+    return right;
+  }
+
+  public void setRight(TreeNode<E> node) {
+    right = node;
+  }
+
+  @Override
+  public String toString() {
+    return value.toString();
+  }
+
+}
